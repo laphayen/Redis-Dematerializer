49a9d2b26cc4f9aca0b976e6cb429ac66f3814a4
Baek Gi Chan <laphayen@gmail.com>
2024-10-08 00:03:43 +0900

    Feat: Add Hash project

---
+++ Data-Type/Hash/src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (var jedisPool = new JedisPool("127.0.0.1", 6379)) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                // hash
+                jedis.hset("users:2:info", "name", "laphayen");
+
+                var userInfo = new HashMap<String, String>();
+                userInfo.put("email", "laphayen@gmail.com");
+                userInfo.put("phone", "010-1234-5678");
+
+                jedis.hset("users:2:info", userInfo);
+
+                jedis.hset("users:2:info", userInfo);
+
+                // hdel
+                jedis.hdel("users:2:info", "phone");
+
+                // get, getall
+                System.out.println(jedis.hget("users:2:info", "email"));
+                Map<String, String> user2Info = jedis.hgetAll("users:2:info");
+                user2Info.forEach((k, v) -> System.out.printf("%s %s%n", k, v));
+
+                // hincr
+                jedis.hincrBy("users:2:info", "visits", 7);
+
+            }
+        }
+    }
+
+}
